8cfed5815edd463ea31c48f01480dbd34677a898
sonesky1 <97257004+sonesky1@users.noreply.github.com>
2025-03-19 23:00:17 -0700

    update readme

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
 # drop-in-the-bucket
-An Environmental Goals Server App: A server that can be run and allow users to enter data and view entries asynchronously on the web. This app uses Rust, Axum, SeaOrm and SQLite. The server_app spins up a localhost web site for now. If this were to be hosted non-locally it would allow multiple users to access the app and add to it asynchronously. 
 
-It creates a database if one does not exist and allows the user to add entries to it while the database is only updated by one client at a time due to Mutex like functionality that is part of SeaOrm.  SQLit was used for this app due to its future adaptability with more complex queries.  
+Application Author:
+Susan Onesky
 
-This will need to be run in a terminal or desktop that can redirect to localhost as this server is on the front end web
-interface on a localhost in how it displays and in how a user will interact with it. It is only local at this time
-This program was run from my PC in Visual Studio. I am unable to run it in the linux terminal on my PC as it does not bring up localhost. However, it does run in terminal on the PSU Ubuntu machines. 
+Project Name:
+Drop in The Bucket - an Environmental Goals Server Application
+
+This app has the focus of allowing multiple users to add a small or large environmental goal they achieved as well as a topic area they could categorize it under. The data a user enters 
+will be stored and when they submit their information it is stored and they view all the data
+that has been entered into the database by everyone that has used the app. This application has a design purpose on the front end of it having a simple user interface. It is not meant to be confusing but rather stick to the point - changing action and sharing actions with others within a community of application users. In theory this application could be run
+locally on any server if the web site address was changed inside the code. Perhaps users
+woudl want to run their own server in a different state. This could help with keeping
+data more specific and pertaining to a city, state or even a group of individuals. 
+
+Another option with the app is to make it cater to larger groups on the web. It could focus
+on being available to anyone in a country, region or even worldwide. The application stores a 
+database locally. If the app were to be hosted on various sites, each site server would have
+its own local database. This could be useful for distributing the database across sites that just want specific users with their own unique data. 
+
+Warning:
+This application does not have any safeguards for use on the WWW right now beyond localhost. It does not have any kind of authentication. As data would be stored locally on a server at this time this could create problems. The application is still under development and for this
+reason has not been made public yet, either. 
+
+The application spins up a server locally that allows adding data and view entries asynchronously on the web.  This app uses Rust, Axum, SeaOrm, tokio and SQLite. If you do not have those installed you will need to do so. The server_app spins up a localhost web site for now. If this were to be hosted non-locally it would allow multiple users to access the app and add to it asynchronously.
+
+The creates a database if one does not exist and allows the user to add entries to it while the database is only updated by one client at a time due to Mutex like functionality that is part of SeaOrm.  In addition, SQLite was used for calling the database. 
+
+This will need to be run in a terminal or desktop that can redirect to localhost as this server visible for now only on the front-end on a local host. 
+
+This program was run from a PC in VS Code. It was also tested on an Ubuntu machine at Portland State University in which I could view localhost.
 
 
 To run the program:
@@ -29,13 +52,12 @@ Did not buy anything at Target today       consumerism
 Bought a used cooking pan at Goodwill      reuse, consumerism
 
 
-If you don't type anything and push the button you will see a response that explains how to use the app.
+If you don't type anything in either or both boxes and push the submit button you will see a response that explains how to use the app.
 
 If you submit your response you will currently be relocated to a page that displays all the input the server has received so far. This is the contents of the database that is stored locally. 
 
-To enter more information you need to return to the previous screen on the browser , delete info in the box you typed or refresh the screen. You can then enter in another input. 
+To enter more information you need to return to the previous screen on the browser , delete info in the box you typed or refresh the screen there on that home page. You can then enter in another input. 
 
-Please ignore the "View responses" button as this will be changed to do what it should do later. It does not yet redirect correcly.  The submit button will also instead just clear the entry boxes on the page once an entry occurs rather than redirecting the user. 
 
 This project is still being worked on. It will store data as lowercase and filter out text that is likely invalid. It will also allow more interesting calls on the data that will have buttons that will be on the view data page to display additional data. Example calls will include:
 View top 5 actions
--- server_app/src/main.rs
+++ server_app/src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{web::Query, Error};
-//adapting this over to using Axum from actix was achieved with help from DeepSeek. 
+//adapting this over to using Axum from actix was achieved with help from DeepSeek.
 use axum::{
-    extract::{Form, State}, response::Html, routing::{get, post}, Router
+    extract::{Form, State},
+    response::Html,
+    routing::{get, post},
+    Router,
 };
+use sea_orm::{ConnectionTrait, Database, DatabaseConnection, DbBackend, DbErr, Statement};
 use serde::Deserialize;
-use sea_orm::{Database, DatabaseConnection, ConnectionTrait, Statement, DbBackend, DbErr};
-use std::sync::Arc;
-//use tokio::sync::Mutex;
 use std::io;
+use std::sync::Arc;
 use tokio::net::TcpListener;
-
 
 #[derive(Deserialize)]
 struct EnvironmentalActionParameters {
@@ -17,54 +17,41 @@ struct EnvironmentalActionParameters {
     topic: String,
 }
 
-#[derive(Deserialize)]
-struct TheData {
-    name: String,
-}
-
-
-/*Define a type for the shared state
-#[derive(Clone)]
-struct AppState {
+async fn retrieve_data(
     db: Arc<DatabaseConnection>,
-}
-*/
+) -> Result<Vec<EnvironmentalActionParameters>, DbErr> {
+    // prep query for SQL
+    let sql = "SELECT action, topic FROM environmental_actions";
+    let statement = Statement::from_string(DbBackend::Sqlite, sql.to_owned());
 
-//adapted from DeepSeek.com AI
-async fn retrieve_data(db: Arc<DatabaseConnection> ) -> Result<Vec<EnvironmentalActionParameters>, DbErr> {
-  // prep query for SQL
-  let sql = "SELECT action, topic FROM environmental_actions";
-  let statement = Statement::from_string(DbBackend::Sqlite, sql.to_owned());
-// Access the shared database state
-  //let db = db.lock().await;
-  let db = db;
-  //execute query and map results to the EnvironmentalActionParameters
-  let data_retrieved = db.query_all(statement)
-  .await?
-  .into_iter()
-  .map(|row| {
-        EnvironmentalActionParameters {
+    let db = db;
+    //execute query and map results to the EnvironmentalActionParameters
+    let data_retrieved = db
+        .query_all(statement)
+        .await?
+        .into_iter()
+        .map(|row| EnvironmentalActionParameters {
             action: row.try_get::<String>("", "action").unwrap(),
             topic: row.try_get::<String>("", "topic").unwrap(),
+        })
+        .collect();
 
-        }
-})
-.collect();
-
-Ok(data_retrieved)
-
+    Ok(data_retrieved)
 }
 
+//receives input from the database and pushes it into a table format in html
+
 fn format_data_as_html(data_retrieved: Vec<EnvironmentalActionParameters>) -> String {
-    let mut to_display   = String::from("<table border='1'><tr><th>Action</th><th>Topic</th></tr>");
+    let mut to_display = String::from("<table border='1'><tr><th>Action</th><th>Topic</th></tr>");
 
     for data in data_retrieved {
         to_display.push_str(&format!(
             "<tr><td>{}</td><td>{}</td></tr>",
-         data.action, data.topic
+            data.action, data.topic
         ));
     }
-    to_display.push_str(&format!("</table>"));
+    
+    to_display.push_str("</table>");
     to_display
 }
 
@@ -72,17 +59,19 @@ async fn add_data_handler(
     State(state): State<Arc<DatabaseConnection>>,
     Form(form): Form<EnvironmentalActionParameters>,
 ) -> Html<String> {
-    if form.action.is_empty() && form.topic.is_empty() {
+    if form.action.is_empty() || form.topic.is_empty() {
+        //if either form is empty then an error message is returned
         return Html(
             "Please enter what you did as your Environmental Action in the first box. \
-            Please type a topic you believe it could belong to in the second box. \n\
-            Action: Rode bike to work    Topic: Transportation".to_string(),
+            Please type a topic you believe it could belong to in the second box. \
+            Action: Rode bike to work    Topic: Transportation"
+                .to_string(),
         );
     }
 
     // Access the shared database state
-    //let db = state.db.lock().await;
-     let db = state.clone();
+
+    let db = state.clone();
     // Example: Insert the action and topic into the database
     let add_to_database = format!(
         "INSERT INTO environmental_actions (action, topic) VALUES ('{}', '{}');",
@@ -93,36 +82,21 @@ async fn add_data_handler(
         .await
         .expect("Failed to insert into environmental_actions table");
 
-    let _response_0 = format!(
-        "Good job. Your action of {} has been added to the environmental bucket under topic {}.",
-        form.action,
-        form.topic,
-    );
-    //let vector_from_database: Vec<EnvironmentalActionParameters> = retrieve_data(&db);
-    //let _vector  = retrieve_data(&db).await;
-    
-    //let the_data_vector: Result<Vec<EnvironmentalActionParameters>, DbErr> = retrieve_data(&db).await;
-     let response: Html<String> = get_data(axum::extract::State(state)).await;
-    //Ok()
-    //let response = format_data_as_html(the_data_vector);
-   // //let data_response = get_data(db);
-    //let response:String = "something".to_string();
+    //gets data from backend if successful and displays it on a redirect
+    //as a table.
+    let response: Html<String> = get_data(axum::extract::State(state)).await;
+
     response
 }
-//my other one
 
-async fn get_data(
-    
-    State(state): State<Arc<DatabaseConnection>>,
-    //Form(query):Form<Query>,
-) -> Html<String>{
+//retrieves data from database
+async fn get_data(State(state): State<Arc<DatabaseConnection>>) -> Html<String> {
     let db = state;
     let data = retrieve_data(db).await.expect("Failure to get data");
     let data_post = format_data_as_html(data);
 
     Html(data_post)
 }
-
 
 async fn get_index() -> Html<&'static str> {
     Html(
@@ -133,28 +107,20 @@ async fn get_index() -> Html<&'static str> {
             <title>Add your Action</title>
         </head>
         <body>
-            <h1>DROP IN THE BUCKET:  Please enter any action you took today to help 
-            the environment in the first box.
-            Please enter a potential category you believe that action could belong
-            to in the second box.
-            Here are some examples: 
-            Action: Rode bike to work                  Topic: Transportation </h1>
+            <h1>DROP IN THE BUCKET: <br \> Please enter any action you took today to help 
+            the environment in the first box. Please enter a potential category you believe that action could belong
+            to in the second box. <br \><br \>
+            EXAMPLE: <br \>
+            Action: Rode bike to work <br \>
+            Topic: Transportation <br/> 
+            </h1>
             <form action="/add_to_database" method="post">
                 <label for="action">Action you did to help the environment:</label><br>
                 <input type="text" id="action" name="action"/><br><br>
-                
                 <label for="topic">Action Category:</label><br>
                 <input type="text" id="topic" name="topic"/><br><br>
-                
                 <button type="submit">Add to Bucket</button>
-            </form>
-
-            <h2>Display Responses</h2>
-            <form action="/get_data" method="post">
-                <button type="submit">Display All Responses</button>
-            </form>
-        
-            
+            </form>            
         </body>
         </html>
         "#,
@@ -180,25 +146,25 @@ async fn main() -> io::Result<()> {
         );
     "#;
 
-    db.execute(Statement::from_string(DbBackend::Sqlite, create_environmental_actions_table_sql.to_string()))
-        .await
-        .expect("Failed to create environmental_actions table");
+    //executes the SQL statement stmt that was created above
+    db.execute(Statement::from_string(
+        DbBackend::Sqlite,
+        create_environmental_actions_table_sql.to_string(),
+    ))
+    .await
+    .expect("Failed to create environmental_actions table");
 
     println!("environmental_actions table in entries_database is set up safely for async!");
 
-    // Use Arc + Mutex to share the database safely in async code
+    // Use Arc to share the database safely in async code
     let state = Arc::new(db);
 
-    // Define the shared state
-    //let state = db.clone();
-     
     // Build the router with the shared state
     let app = Router::new()
         .route("/", get(get_index))
         .route("/add_to_database", post(add_data_handler))
-        //.route("/query", post(get_data))
-        .with_state(state.into());
-       
+        .with_state(state);
+        //.with_state(state.into());
 
     // Serve the app
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
@@ -207,45 +173,3 @@ async fn main() -> io::Result<()> {
 
     Ok(())
 }
-    //let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    //println!("Serving on http://{}...", addr);
-
-    /*axum::serve(&app)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-    */
-
-/*
-fn _separate_str_by_word{
-
-
-}
-
-fn _remove_punctuation{
-
-
-
-}
-
-fn _remove_common_non_verbs{
-
-
-}
-
-fn _create_dictionary_of_common_not_helfpul_words{
-//a, the, we, you, them, (this will help in only storing actions by other probably more "important" words for keeping track of action points)
-
-}
-
-fn _ability_for_someone_to_comment{
-  //dream for later
-
-}
-
-fn _show_random_entries(u8){
-//program will pick random entries from list
-
-}
-
-*/
